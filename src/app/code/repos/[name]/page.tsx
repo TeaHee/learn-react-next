7d6945d2dacb7133f4bd14c3dde6c409b3581cf7
TeaHee <tiger92tn@gmail.com>
2023-05-04 15:30:23 +0700

    hiển thị loading cho từng component trong khi chờ api trả dữ liệu

--- src/app/code/repos/[name]/page.tsx
+++ src/app/code/repos/[name]/page.tsx
@@ -2,6 +2,7 @@ import Repo from '@/app/components/repo'
 import RepoDirs from '@/app/components/repoDirs'
 import Link from 'next/link'
 import React from 'react'
+import { Suspense } from 'react'
 
 // dynamic router
 // đặt tên thư mục có tên [name]
@@ -12,11 +13,17 @@ const RepoPage = ({params}: any) => {
     <div className='card'>
       <Link href={'/code/repos'} className='btn btn-back'>Back to Repositories</Link>
 
-      {/* @ts-expect-error Server Component */}
-      <Repo name={params.name} />
+      {/* Xử lí việc data chưa được trả về ngay lập tức, tương tự như spinner */}
+      <Suspense fallback={<div>Loading repo...</div>}>
+        {/* @ts-expect-error Server Component */}
+        <Repo name={params.name} />
+      </Suspense>
 
-      {/* @ts-expect-error Server Component */}
-      <RepoDirs name={params.name} />
+      {/* Sử dụng được cho từng component riêng hoặc chung cho tất cả component*/}
+      <Suspense fallback={<div>Loading directories...</div>}>
+        {/* @ts-expect-error Server Component */}
+        <RepoDirs name={params.name} />
+      </Suspense>
     </div>
   )
 }
