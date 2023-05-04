0a1e6742ad8c8c144d2a8bf2fda2322dcd751e66
TeaHee <tiger92tn@gmail.com>
2023-05-04 10:55:07 +0700

    render dữ liệu lên html

--- src/app/code/repos/page.tsx
+++ src/app/code/repos/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link';
+import {FaStar, FaCodeBranch, FaEye} from 'react-icons/fa'
 
 async function fetchData() {
     const response = await fetch('https://api.github.com/users/bradtraversy/repos')
@@ -10,7 +12,24 @@ const ReposPage = async () => {
     const response = await fetchData()
 
   return (
-    <div>{response[0].name}</div>
+    <div className='repos-container'>
+        <h2>Repositories</h2>
+        <ul className="repo-list">
+            {response.map((data: any) => (
+                <li key={data.id}>
+                    <Link href={`/code/repos/${data.name}`}>
+                        <h3>{data.name}</h3>
+                        <p>{data.description}</p>
+                        <div className="repo-details">
+                            <span><FaStar /> {data.stargazers_count}</span>
+                            <span><FaCodeBranch /> {data.forks_count}</span>
+                            <span><FaEye /> {data.watchers_count}</span>
+                        </div>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
