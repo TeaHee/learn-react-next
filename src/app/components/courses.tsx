b73867db5a87d49141b807fddb51fbb2760137cc
TeaHee <tiger92tn@gmail.com>
2023-05-05 09:45:27 +0700

    Fix err enviroment when deploy

--- src/app/components/courses.tsx
+++ src/app/components/courses.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-async function fetchCourese() {
-    console.log("url::", `${process.env.BASE_URL_LOCAL}`)
-    const request = await fetch(`${process.env.BASE_URL_LOCAL}/api/courses`)
+async function fetchCourses() {
+    console.log("url::", `${process.env.BASE_URL}`)
+    const request = await fetch(`${process.env.BASE_URL}/api/courses`)
     const courses = await request.json();
     return courses;
 }
 
 const courses = async () => {
-    const courses = await fetchCourese()
+    const courses = await fetchCourses()
   return (
     <div className='courses'>
         {courses.map((data:any) => (
